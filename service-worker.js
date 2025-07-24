bcf1c8be9b646196c1c978a03752349e08399e00
Akbari5561 <iranposter009@gmail.com>
2025-07-24 13:33:03 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-const CACHE_NAME = 'my-pwa-cache-v6.7'; // شماره نسخه جدیدتر
+const CACHE_NAME = 'my-pwa-cache-v6.8'; // شماره نسخه جدیدتر
 const urlsToCache = [
   '/',
   '/index.html',
